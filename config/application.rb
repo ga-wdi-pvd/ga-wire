0ffc37dfaae1758366dc40711b39698df349f3c7
estermer <eric.stermer@gmail.com>
2017-02-07 16:31:53 -0500

    removed github client_id and secret from comments

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,5 @@ module GaWireApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.middleware.use OmniAuth::Builder do
-    #   # provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']#, scope: "user:email,user:follow"
-    #   provider :github, '383adfc3974a7f515f61', 'a8a77a97033efc786dfe3fe779cc773e9b321d5c'
-    # end
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, Rails.application.secrets.github_key, Rails.application.secrets.github_secret #, scope: "user:email,user:follow"
-  # provider :github, '383adfc3974a7f515f61', 'a8a77a97033efc786dfe3fe779cc773e9b321d5c'
 end
