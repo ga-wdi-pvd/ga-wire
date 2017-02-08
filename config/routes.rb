b36b61119156a0ec2b9b615c24e3272ae86275d9
estermer <eric.stermer@gmail.com>
2017-02-08 16:33:22 -0500

    made :index and execption to post resources in rails routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, except: [:new, :edit]
+  resources :posts, except: [:index, :new, :edit]
 
   get "auth/github/", to: "welcome#index"
   match "auth/github/callback" => "sessions#create", :via => [:get, :post]
