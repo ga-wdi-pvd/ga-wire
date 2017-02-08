f7511820c81ea975b0076732d46ae6f8c212e0a5
estermer <eric.stermer@gmail.com>
2017-02-08 15:13:06 -0500

    added props for the current user

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -1,10 +1,11 @@
 class App extends React.Component {
   render () {
-    let user = this.props;
+    let {user} = this.props;
     return (
       <div>
         <a href="/auth/github">Login/Signup</a>
         <h1>{user.nickname}</h1>
+        <img src={user.image_url}/>
       </div>
     );
   }
