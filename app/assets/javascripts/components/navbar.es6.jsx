0c142b2f88ea3c12d42e89f1d07015f435d5add3
estermer <eric.stermer@gmail.com>
2017-02-08 15:44:52 -0500

    redirected logout method to root_path
    added another navlink component to the navbar for logout

--- app/assets/javascripts/components/navbar.es6.jsx
+++ app/assets/javascripts/components/navbar.es6.jsx
@@ -1,11 +1,17 @@
 class Navbar extends React.Component {
   render () {
     let links = [
-      {link: '/auth/github', name: 'Signup/Login'}
+      {link: '/auth/github', name: 'Signup/Login'},
+      {link: '/logout', name: 'Logout'}
     ];
     return (
       <div>
-        <Navlink />
+        <Navlink
+          link={links[0].link}
+          linkName={links[0].name}/>
+        <Navlink
+          link={links[1].link}
+          linkName={links[1].name}/>
       </div>
     );
   }
--- app/assets/javascripts/components/navlink.es6.jsx
+++ app/assets/javascripts/components/navlink.es6.jsx
@@ -2,7 +2,7 @@ class Navlink extends React.Component {
   render () {
     let {link, linkName} = this.props;
     return (
-      <a href="/auth/github">Signup/Login</a>
+      <a href={link}>{linkName}</a>
     );
   }
 }
