5f73850876625c65102ff4e2fff9dc674a4485d2
estermer <eric.stermer@gmail.com>
2017-02-08 22:45:49 -0500

    added submit button to the form

--- app/assets/javascripts/components/post_form.es6.jsx
+++ app/assets/javascripts/components/post_form.es6.jsx
@@ -5,6 +5,7 @@ class PostForm extends React.Component {
         <form action="/posts" method="POST">
           <input type="text" name="title"/><br>
           <input type="text" name="body"/><br>
+          <input type="submit" value="Submit Post"/>
         </form>
       </div>
     );
