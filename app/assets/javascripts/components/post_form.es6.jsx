9bde56b132c07033b958b57ef855ee8abc17011e
estermer <eric.stermer@gmail.com>
2017-02-08 23:55:05 -0500

    readded user_id to permit for strong params in poast controller
    hard coded a hiddent input type with a user id of 1 in the postform
    
    successful post creation

--- app/assets/javascripts/components/post_form.es6.jsx
+++ app/assets/javascripts/components/post_form.es6.jsx
@@ -3,6 +3,7 @@ class PostForm extends React.Component {
     return (
       <div>
         <form action="/posts" method="POST">
+          <input type="hidden" name="post[user_id]" value="1"/>
           <label>Title of your post:</label><br/>
           <input type="text" name="post[title]"/><br/>
           <label>What would you like to write?</label><br/>
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.required(:post).permit(:title, :body)
+    params.required(:post).permit(:user_id, :title, :body)
   end
 
 end
