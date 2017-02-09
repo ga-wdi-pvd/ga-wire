1613971ec5da61226f330267ecfea978bc70f787
estermer <eric.stermer@gmail.com>
2017-02-09 12:47:35 -0500

    added react_component to the welcome index erb with the post list
    created an instance variable @posts = Post.all in index method for welcome controller

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -4,7 +4,6 @@ class App extends React.Component {
     return (
       <div>
         <PostForm />
-        <PostList posts={posts}/>
         <Post post={post}/>
       </div>
     );
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    # posts = Post.all
+    @posts = Post.all
     # render component: 'App', props: { user: {}, posts: posts, post: {}}
   end
 
