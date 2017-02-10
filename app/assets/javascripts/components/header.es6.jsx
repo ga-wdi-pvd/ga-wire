b876fed2282f0e5ddd6007dd044bb199da5307ba
estermer <eric.stermer@gmail.com>
2017-02-10 12:06:27 -0500

    created a header-logo component

--- app/assets/javascripts/components/header.es6.jsx
+++ app/assets/javascripts/components/header.es6.jsx
@@ -1,11 +1,9 @@
 class Header extends React.Component {
   render () {
     let {user, img} = this.props;
-    console.log(img);
     return (
       <div className="header">
-        <img src={img} alt="logo"/>
-        <h1>GA Wire</h1>
+        <HeaderLogo />
         <Navbar />
         <Avatar img={user.image_url}/>
       </div>
---
+++ app/assets/javascripts/components/header_logo.es6.jsx
@@ -0,0 +1,10 @@
+class HeaderLogo extends React.Component {
+  render () {
+    return (
+      <div>
+        <img src={img} alt="logo"/>
+        <p>WIRE</p>
+      </div>
+    );
+  }
+}
