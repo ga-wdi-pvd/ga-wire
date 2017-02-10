6606478a7d8136366556d5fce6741e33fca64a93
estermer <eric.stermer@gmail.com>
2017-02-10 11:33:16 -0500

    added the ga logo to the header

--- app/assets/javascripts/components/header.es6.jsx
+++ app/assets/javascripts/components/header.es6.jsx
@@ -1,8 +1,10 @@
 class Header extends React.Component {
   render () {
-    let {user} = this.props;
+    let {user, img} = this.props;
+    console.log(img);
     return (
       <div>
+        <img src={img} alt="logo"/>
         <h1>GA Wire</h1>
         <Navbar />
         <h1>{user.nickname}</h1>
