bf139e6f7165d19d568d480cfd073bdf5bb7c606
estermer <eric.stermer@gmail.com>
2017-02-09 22:57:07 -0500

    added a new post link in the posts list component

--- app/assets/javascripts/components/post_list.es6.jsx
+++ app/assets/javascripts/components/post_list.es6.jsx
@@ -2,6 +2,7 @@ class PostList extends React.Component {
   render () {
     return (
       <div>
+        <a href="/posts/new">&#43; New Post</a>
         {this.props.posts.map((post, index) => {
           return (<PostItem post={post}/>);
         })}
