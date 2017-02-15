3bff171de59406498e282b45f888a9b6ec5d7b1f
estermer <eric.stermer@gmail.com>
2017-02-15 10:38:49 -0500

    changed the way the date was displayed to a more human readable format

--- app/assets/javascripts/components/post_item.es6.jsx
+++ app/assets/javascripts/components/post_item.es6.jsx
@@ -2,7 +2,7 @@ class PostItem extends React.Component {
   render () {
     let {post, user} = this.props;
     let cts = this.props.post.created_at;
-    let cdate = (new Date(cts)).toString();
+    let cdate = (new Date(cts)).toDateString();
     return (
       <tr>
         <td>
