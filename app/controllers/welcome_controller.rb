cfcc98093ed28baa1d6414d42cc0dd9f64a02d38
estermer <eric.stermer@gmail.com>
2017-02-15 13:24:51 -0500

    removed bye bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @posts = Post.includes(:user).reverse.map {|post| post.attributes.merge!({user: {image_url: post.user.image_url, nickname: post.user.nickname}})}
-    byebug
     render component: 'PostList', props: {posts: @posts}
   end
 end
