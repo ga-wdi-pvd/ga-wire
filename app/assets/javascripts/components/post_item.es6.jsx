630eaa46d89217f949d7f6f12e144c90b646233e
estermer <eric.stermer@gmail.com>
2017-02-15 10:23:31 -0500

    reorganized the html for the post list to take advantage of bootstraps table

--- app/assets/javascripts/components/post_item.es6.jsx
+++ app/assets/javascripts/components/post_item.es6.jsx
@@ -2,11 +2,13 @@ class PostItem extends React.Component {
   render () {
     let {post} = this.props;
     return (
-      <div className="post-item">
-        <a href={`/posts/${post.id}`}>
-          <h4>{post.title}</h4>
-        </a>
-      </div>
+      <tr>
+        <td>
+          <h4><a href={`/posts/${post.id}`}>{post.title}</a></h4>
+        </td>
+        <td class="cell-stat text-center hidden-xs hidden-sm"><a href="#">John Doe</a></td>
+        <td class="cell-stat text-center hidden-xs hidden-sm"><a href="#">12/12/2017</a></td>
+      </tr>
     );
   }
 }
--- app/assets/javascripts/components/post_list.es6.jsx
+++ app/assets/javascripts/components/post_list.es6.jsx
@@ -2,13 +2,41 @@ class PostList extends React.Component {
   render () {
     return (
       <div className="container">
-        <a className="new-button" href="/posts/new">&#43; New Post</a>
-        <ListHeader />
-        <hr />
-        {this.props.posts.map((post, index) => {
-          return (<PostItem post={post}/>);
-        })}
+        <table className="table forum table-striped">
+          <a className="new-button" href="/posts/new">&#43; New Post</a>
+          <ListHeader />
+          <tbody>
+            {this.props.posts.map((post, index) => {
+              return (<PostItem post={post}/>);
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
 }
+
+// <table class="table forum table-striped">
+//     <thead>
+//       <tr>
+//         <th class="cell-stat"></th>
+//         <th>
+//           <h3>Important</h3>
+//         </th>
+//         <th class="cell-stat text-center hidden-xs hidden-sm">Topics</th>
+//         <th class="cell-stat text-center hidden-xs hidden-sm">Posts</th>
+//         <th class="cell-stat-2x hidden-xs hidden-sm">Last Post</th>
+//       </tr>
+//     </thead>
+//     <tbody>
+//       <tr>
+//         <td class="text-center"><i class="fa fa-question fa-2x text-primary"></i></td>
+//         <td>
+//           <h4><a href="#">Frequently Asked Questions</a><br><small>Some description</small></h4>
+//         </td>
+//         <td class="text-center hidden-xs hidden-sm"><a href="#">9 542</a></td>
+//         <td class="text-center hidden-xs hidden-sm"><a href="#">89 897</a></td>
+//         <td class="hidden-xs hidden-sm">by <a href="#">John Doe</a><br><small><i class="fa fa-clock-o"></i> 3 months ago</small></td>
+//       </tr>
+//     </tbody>
+//   </table>
