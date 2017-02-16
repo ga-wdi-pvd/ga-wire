ca1fc1d5b096d6e32aeb04213744f855d498511f
estermer <eric.stermer@gmail.com>
2017-02-16 13:08:56 -0500

    uncommented all the builder and user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authorize!, only: [:show]
 
   def show
-    # user = Builder.data(current_user)
+    user = Builder.data(current_user)
     # future dashboard for viewing a user profile component
   end
 
